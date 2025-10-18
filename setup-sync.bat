e7645ae4a7e6510c2dca907f924fe80e59a1c8d9
TonyToCode <anton.ovsinskiy@russianschool.com>
2025-10-18 22:35:17 +0400

    Delete setup-sync.bat

--- setup-sync.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Настройка автоматической синхронизации с GitHub...
-powershell -ExecutionPolicy Bypass -File "%~dp0setup-sync.ps1"
-echo.
-echo Готово! Теперь файлы будут синхронизироваться каждые 5 минут.
-echo.
-pause
