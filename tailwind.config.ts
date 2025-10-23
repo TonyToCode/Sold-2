72e837a052afd2e3c982bee97fccc8627f303c32
Sold Website <sold@example.com>
2025-10-23 14:59:35 +0400

    fix: убрал все ссылки на src/ из конфигов

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,6 @@ export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     container: {
